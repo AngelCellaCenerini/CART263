1e591945b1e131a5528e34f1190ee87f4ecde009
Angel CC <angelcenerini@gmail.com>
2021-01-20 20:26:35 -0500

    E01: Added Animals to program
    - Preloaded png files;
    - Set up() + Draw();

--- Exercises/Exercise01/js/script.js
+++ Exercises/Exercise01/js/script.js
@@ -9,10 +9,21 @@ Here is a description of this template p5 project.
 
 **************************************************/
 
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
 // preload()
 //
 // Description of preload goes here.
 function preload() {
+
+  for(let i = 0; i < NUM_ANIMAL_IMAGES; i++){
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
 
 }
 
@@ -21,11 +32,27 @@ function preload() {
 // Description of setup() goes here.
 function setup() {
 
+  createCanvas(windowWidth, windowHeight);
+
+  // Create Animals
+  for(let i = 0; i < NUM_ANIMALS; i++){
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+background(148, 221, 185); // greenish background
+
+// Animals
+for(let i = 0; i < animals.length; i ++){
+ animals[i].update();
+}
 
 }
