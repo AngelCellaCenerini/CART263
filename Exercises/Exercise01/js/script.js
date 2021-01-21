b06803b706339250b94d96d70a6a28aadfd51e74
Angel CC <angelcenerini@gmail.com>
2021-01-20 19:40:39 -0500

    E01: Started Exercise 01
    Where’s Sausage Dog? New Game+

---
+++ Exercises/Exercise01/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**************************************************
+
+Exercise 01 -  Where’s Sausage Dog? New Game+ (p5 Template by CART253 course)
+Angel Cella Cenerini
+
+Here is a description of this template p5 project.
+
+**************************************************/
+
+// preload()
+//
+// Description of preload goes here.
+function preload() {
+
+}
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
