9f7c751a1ba3f6c4959bd39a4e8ba46990414b42
Angel CC <angelcenerini@gmail.com>
2021-02-06 21:06:07 -0500

    E03: Set up Default Profile
    Program 'prompt's User's name

--- Exercises/Exercise03/js/script.js
+++ Exercises/Exercise03/js/script.js
@@ -8,6 +8,13 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`
+};
+
 /**
 Description of preload
 */
@@ -20,7 +27,8 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+createCanvas(windowWidth, windowHeight);
+spyProfile.name = prompt(`Identify yourself.`);
 }
 
 
@@ -28,5 +36,23 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(0);
 
+let profile = `**AGENT IDENTIFICATION**
+
+Name: ${spyProfile.name}
+
+Alias: ${spyProfile.alias}
+
+Secret Weapon: ${spyProfile.secretWeapon}
+
+Password: ${spyProfile.password}`;
+
+push();
+fill(255);
+textSize(22);
+textFont(`Courier`);
+textAlign(LEFT, TOP);
+text(profile, 100, 100);
+pop();
 }
