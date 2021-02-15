bba4c26c0f4aee7f4e27e08ed0416ac09fd2b752
Angel CC <angelcenerini@gmail.com>
2021-02-14 23:02:38 -0500

    P01D: Experimenting

---
+++ Projects/Project01Draft/js/Credits.js
@@ -0,0 +1,30 @@
+class Credits{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.string = `"You have to treat it almost like a little animal; it's good at certain things and not good at certain other things."
+    
+
+    "We can do a Pinch Siulator now - pinch, pinch!"`;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = -1;
+  }
+
+  update(){
+    this.move();
+    this.display();
+  }
+
+  move(){
+   this.y += this.vy;
+   this.vy = this.speed;
+  }
+
+  display(){
+    push();
+    fill(255);
+    text(this.string, this.x, this.y);
+    pop();
+  }
+}
--- Projects/Project01Draft/js/script.js
+++ Projects/Project01Draft/js/script.js
@@ -8,13 +8,22 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
-let state = `carRide`; // Title, Intro/Instructions, CarRide, CutScene, Chase, BadEnding(01, 02), JurassicParkMoment, PetDino, Selfie, Credits
+
+
+
+// Blood Splatter
+let bloodSplatterImage = undefined;
+
+// Credits String
+let credits = undefined;
+
+let state = `credits`; // Title, Intro/Instructions, CarRide, CutScene, Chase, BadEnding(01, 02), JurassicParkMoment, PetDino, Selfie, Credits
 
 /**
 Description of preload
 */
 function preload() {
-
+bloodSplatterImage = loadImage(`assets/images/splatter.png`)
 }
 
 
@@ -26,8 +35,16 @@ function setup() {
 createCanvas(windowWidth, windowHeight);
 textFont(`Courier`);
 textAlign(CENTER, CENTER);
+textSize(20);
 rectMode(CENTER);
+imageMode(CENTER);
 noStroke();
+
+
+// Create Credits String
+let x = width/2;
+let y = 3*height/2;
+credits = new Credits(x, y);
 
 
 
@@ -43,94 +60,205 @@ function draw() {
 
   if (state === `title`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`JURASSIC PARK...`, width/2, height/7);
-    text(`...MOMENT.`, width/2, 6*height/7);
-    pop();
+    titleText();
 
   }
   else if (state === `intro`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`You embark on your journey in search of the Jurassic Park Moment.`, width/2, height/7);
-    text(`Among the lush vegetation, you hear a sound...Type it down as to document it.`, width/2, 6*height/7);
-    pop();
+    introText();
 
   }
   else if (state === `carRide`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`You keep the radio on while travelling. Press the arrow key > to skip channel frequencies. Press ENTER to skip this level.`, width/2, height/7);
-    textStyle(ITALIC);
-    text(`<<Country roads, take me home...>>`, width/2, 6*height/7);
-    pop();
+    carRideText();
 
   }
   else if (state === `cutScene`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`Looks like all that noise attracted unwanted attention!`, width/2, height/7);
-    text(`Quick! Press G to step on the gas!`, width/2, 6*height/7);
-    pop();
+    cutSceneText();
 
   }
   else if (state === `chase`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`You embark on your journey in search of the Jurassic Moment.`, width/2, height/7);
-    text(`Among the lush vegetation, you hear a sound...`, width/2, 6*height/7);
-    pop();
+    chaseText();
 
   }
   else if (state === `badEnding01`){
 
+    // Image Background (Blood Splatter)
+    image(bloodSplatterImage, 2*width/3, 2*height/3);
+
+    badEnding01Text();
+
   }
   else if (state === `jurassicParkMoment`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`You embark on your journey in search of the Jurassic Moment.`, width/2, height/7);
-    text(`Among the lush vegetation, you hear a sound...`, width/2, 6*height/7);
-    pop();
+    jurassicParkMomentText();
 
   }
   else if (state === `petDino`){
 
-    push();
-    fill(255);
-    rect(width/2, height/2, 1000, 530);
-    textSize(20);
-    text(`You embark on your journey in search of the Jurassic Moment.`, width/2, height/7);
-    text(`Among the lush vegetation, you hear a sound...`, width/2, 6*height/7);
-    pop();
+    petDino();
 
   }
   else if (state === `badEnding02`){
 
+    // Image Background (Blood Splatter)
+    image(bloodSplatterImage, 2*width/3, 2*height/3);
+
+    badEnding02Text();
+
   }
   else if (state === `selfie`){
+
+    selfieText();
 
   }
   else if (state === `credits`){
 
+    credits.update();
+
   }
 
+
+}
+
+// Title
+function titleText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  textSize(40);
+  text(`JURASSIC PARK...`, width/2, height/7);
+  text(`...MOMENT.`, width/2, 6*height/7);
+  pop();
+
+}
+
+// Intro
+function introText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  text(`You embark on your journey in search of the Jurassic Park Moment.`, width/2, height/7);
+  text(`Among the lush vegetation, you hear a sound...Type it down as to document it.`, width/2, 6*height/7);
+  pop();
+
+}
+
+// Car Ride
+function carRideText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  text(`You keep the radio on while travelling. Press the arrow key > to skip channel frequencies. Press ENTER to skip this level.`, width/2, height/7);
+  textStyle(ITALIC);
+  text(`<<Country roads, take me home...>>`, width/2, 6*height/7);
+  pop();
+
+}
+
+// Cut Scene
+function cutSceneText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  text(`Looks like all that noise attracted unwanted attention!`, width/2, height/7);
+  text(`Quick! Press G to step on the gas!`, width/2, 6*height/7);
+  pop();
+}
+
+// Chase
+function chaseText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  text(`You embark on your journey in search of the Jurassic Moment.`, width/2, height/7);
+  text(`Among the lush vegetation, you hear a sound...`, width/2, 6*height/7);
+  pop();
+}
+
+// Bad Ending 01
+function badEnding01Text(){
+  push();
+  fill(255);
+  text(`You weren't quick enough and got just too close to the dinosaur :/`, width/2, height/2);
+  pop();
+
+}
+
+// Jurassic Park Moment
+function jurassicParkMomentText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  text(`You're heart is booming in your your chest! Could this be it?`, width/2, height/7);
+  text(`Is this...the legendary jUraSsIc PaRK mOmENt?!`, width/2, 6*height/7);
+  pop();
+
+}
+
+// Pet Dino
+function petDinoText(){
+  push();
+  fill(255);
+  rect(width/2, height/2, 1000, 530);
+  text(`Wow, it's gotten really close! It's as if you could lift your hand in front of your webcam and try petting it...`, width/2, height/7);
+  text(`...Unless that might not be the brightest idea? You could also wait for it to pass...`, width/2, 6*height/7);
+  pop();
+
+}
+
+// Bad Ending 02
+function badEnding02Text(){
+  push();
+  fill(255);
+  text(`You...really thought petting a dinosaur would be a good idea?`, width/2, height/2);
+  pop();
+
+}
+
+// Selfie
+function selfieText(){
+  push();
+  fill(255);
+  text(`Wise choice! Why not take a picture to commemorate your accomplished mission?`, width/2, 6*height/7);
+  pop();
+
+}
+
+// Credits
+// function selfieText(){
+//
+// }
+
+function keyPressed(){
+
+  // User presses ENTER
+  if(keyCode === 13){
+    if(state === `title`){
+      state = `intro`;
+    }
+    else if(state === `intro`){
+      state = `carRide`;
+    }
+    else if(state === `carRide`){
+      state = `cutScene`;
+    }
+    else if(state === `selfie`){
+      state = `credits`;
+    }
+
+  }
+
+  // User presses Right Arrow
+  if(keyCode === 39 && state === `carRide`){
+
+  }
+
+  // User presses G
+  if(keyCode === 71 && state === `cutScene`){
+    state = `chase`;
+  }
 
 }
