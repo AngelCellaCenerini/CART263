5dfa011ed616bd78abb36ac5d3a7bfcf5a872b9e
Angel CC <angelcenerini@gmail.com>
2021-03-09 17:23:45 -0500

    E05: Improved/Finalized Exercise 05
    - Organized program in functions + added comments;
    - Introduced more HTML elements animated via CSS;
    - Added Keyboard Input;
    - Added Sound/Image files;
    
    Hopefully the code is not too messy

--- Exercises/Exercise05/js/script.js
+++ Exercises/Exercise05/js/script.js
@@ -2,12 +2,12 @@
 Haiku Generator++
 Angel Cella Cenerini
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Unquestionable Haiku(s) 'til "night time".
 */
 
 "use strict";
 
+// Haiku Lines
 let fiveSyllableLines = [
   `Oh, to have money`,
   `The rainbow doesn't know`,
@@ -29,29 +29,88 @@ let sevenSyllableLines = [
   `Fear not the approaching duck`
 ];
 
-let line1 = random(fiveSyllableLines);
-let line2 = random(sevenSyllableLines);
-let line3 = random(fiveSyllableLines);
+// Title
+let titles = [
+  `The Jolly Clown`,
+  `Gmail Needs You`,
+  `Stormy Letters.`,
+  `Fallen Windows.`,
+  `A Crippled Swan`,
+  `The Watery Suns`
+];
 
+
+// Grabe Elements from page
+// Page's Backgorund
+let background = document.body
+// Title
+let titleH = document.getElementById(`title`);
+// Haiku Lines
 let line1P = document.getElementById(`line-1`);
 let line2P = document.getElementById(`line-2`);
 let line3P = document.getElementById(`line-3`);
+// Directions
+let direction1P = document.getElementById(`direction-1`);
+let direction2P = document.getElementById(`direction-2`);
 
-line1P.innerText = line1;
-line2P.innerText = line2;
-line3P.innerText = line3;
+// SFX
+const crickets = new Audio("assets/sounds/crickets.mp3");
 
-line1P.addEventListener(`click`, lineClicked);
-line2P.addEventListener(`click`, lineClicked);
-line3P.addEventListener(`click`, lineClicked);
+// Set Up
+// Generate Titled Haiku
+generateTitle();
+generateLines();
+// User Input
+changeLine();  // mouse
+nightComes();  // keyboard
 
 
+function generateTitle(){
+  // Set Up Title
+  let title = random(titles);
+  titleH.innerText = title;
+}
+function generateLines(){
+  // Set Up Lines
+  let line1 = random(fiveSyllableLines);
+  let line2 = random(sevenSyllableLines);
+  let line3 = random(fiveSyllableLines);
+
+  line1P.innerText = line1;
+  line2P.innerText = line2;
+  line3P.innerText = line3;
+}
+
+function changeLine(){
+  // User Input affects page
+  line1P.addEventListener(`click`, lineClicked);
+  line2P.addEventListener(`click`, lineClicked);
+  line3P.addEventListener(`click`, lineClicked);
+}
+
+function nightComes(){
+  // User Input affects page
+  background.addEventListener(`keydown`, pressedKeyDown);
+  titleH.addEventListener(`keydown`, pressedKeyDown);
+  line1P.addEventListener(`keydown`, pressedKeyDown);
+  line2P.addEventListener(`keydown`, pressedKeyDown);
+  line3P.addEventListener(`keydown`, pressedKeyDown);
+}
+// /Set Up
+
+
+function random(array){
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+// Mouse Input
 function lineClicked(event){
   fadeOut(event.target, 1);
-
 }
 
 function fadeOut(element, opacity){
+  // Fade Out Line
   opacity -= 0.01;
   element.style[`opacity`] = opacity;
   if (opacity > 0){
@@ -65,7 +124,17 @@ function fadeOut(element, opacity){
   }
 }
 
+function setNewLine(element){
+  if (element === line1P || element === line3P){
+    element.innerText = random(fiveSyllableLines);
+  }
+  else if (element === line2P){
+    element.innerText = random(sevenSyllableLines);
+  }
+}
+
 function fadeIn(element, opacity){
+  // Fade In Line
   opacity += 0.01;
   element.style[`opacity`] = opacity;
   if (opacity < 1){
@@ -74,17 +143,39 @@ function fadeIn(element, opacity){
     });
   }
 }
+//
 
-function setNewLine(element){
-  if (element === line1P || element === line2P){
-    element.innerText = random(fiveSyllableLines);
-  }
-  else if (element === line3P){
-    element.innerText = random(sevenSyllableLines);
-  }
+// Keyboard Input
+function pressedKeyDown(){
+  // User Input affects page
+  changeElementsColor();
+  changeElementsText();
+
+  crickets.play();  // play Crickets SFX
 }
 
-function random(array){
-  let index = Math.floor(Math.random() * array.length);
-  return array[index];
+function changeElementsColor(){
+  // Change page backgorund color to black
+  background.style.backgroundColor = `#000000`;
+  // Change text color to white
+  titleH.style.color = `#ffffff`;
+  line1P.style.color = `#ffffff`;
+  line2P.style.color = `#ffffff`;
+  line3P.style.color = `#ffffff`;
+  // Directions
+  direction1P.style.color = `#ffffff`;
+  direction2P.style.color = `#ffffff`;
 }
+function changeElementsText(){
+  // Set page text
+  // Title
+  titleH.innerText = `Dark has fallen`;
+  // Lines
+  line1P.innerText = `Clouds are no longer`;
+  line2P.innerText = `This could never be a meme`;
+  line3P.innerText = `Why did the fish speak`;
+  // Directions
+  direction1P.innerText = `You have chosen to end things...`;
+  direction2P.innerText = `Okay. Do you like the crickets?`;
+}
+//
