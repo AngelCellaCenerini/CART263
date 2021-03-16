c145a527832fc6a65b53c4e107756c6d2eb75ecd
Angel CC <angelcenerini@gmail.com>
2021-03-16 13:14:49 -0400

    E06: Started working on Exercise06
    - Inserted basic description/info
    - Included jQuery library

---
+++ Exercises/Exercise06/js/script.js
@@ -0,0 +1,11 @@
+/**
+Exercise 06: Raving Redactionist++
+Angel Cella Cenerini
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+// Code goes here
