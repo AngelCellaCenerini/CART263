df243c5303601b462c03b53c854ccbe012a0a702
Angel CC <angelcenerini@gmail.com>
2021-03-23 00:16:56 -0400

    E07: Improved and Finalized Program
    - Organized/commented program
    - Improved HTML/CSS
    - Changed (ahem, wrote myself XD) text and thus secret message
    - included Instruction Dialog
    - included SFX
    - included "ending" of sorts (final background image - stars gif).

--- Exercises/Exercise07/js/script.js
+++ Exercises/Exercise07/js/script.js
@@ -1,38 +1,70 @@
 /**
-Title of Project
-Author Name
+We're all tired, and that's okay
+Angel Cella Cenerini
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is a poem of sorts; perhaps not very poetic, but hopefully relatable.
+Is our mind too drained to discover the secret message within?
 */
 
 "use strict";
 
-$(`.secret`).one(`mouseover`, function(event) {
-  $(this).addClass(`found`, 500);
-  $(this).draggable({
-    helper: `clone`
-  });
-});
+// SFX
+const birthdayBlowerSFX = new Audio("assets/sounds/birthdayBlower.mp3");
+const lullabySFX = new Audio("assets/sounds/lullaby.mp3");
 
-$(`#answer`).droppable({
-  drop: function(event, ui){
-    let letter = ui.draggable.text();
-    $(this).append(letter);
-    ui.draggable.draggable(`disable`);
-    ui.draggable.removeClass(`found`);
-    // Check
-    if( $(this).text() === `Theremin`){
-      $(`#solved-dialog`).dialog(`open`);
+// Secret Message
+const secretAnswer = `sleepDeprivation`;
+
+// Display Istructions (or rather, make User read Instructions)
+$(`#exercise-instructions`).dialog({
+  modal: true,
+  resizable: false,
+  buttons: {
+    // Confirm Instructions, start simulation
+    "Understood(?)": function(){
+      $(this).dialog(`close`);
     }
   }
 });
 
+// Discover Letters
+$(`.secret`).one(`mouseover`, function(event) {
+  // Display Underlined and Bold letters via Mouse Input
+  $(this).addClass(`found`, 500);
+  // Make letters draggable
+  $(this).draggable({
+    // Clone dragged letters
+    helper: `clone`
+  });
+});
+
+// Set Up Droppable Element
+$(`#answer`).droppable({
+  drop: function(event, ui){
+    // PLace letter in Draggable Element
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    // Check if User inserted correct Secret Answer
+    if( $(this).text() === secretAnswer){
+      // Congratulate User - Open Dialog
+      $(`#solved-dialog`).dialog(`open`);
+      birthdayBlowerSFX.play(); // SFX
+    }
+  }
+});
+
+// Final Dialog
 $(`#solved-dialog`).dialog({
   autoOpen: false,
+  resizable: false,
   buttons: {
-    "I know": function(){
+    "zzz": function(){
       $(this).dialog(`close`);
+      // Turn Background Image Visible
+      $(`#background-image`).css(`visibility`, `visible`);
+      lullabySFX.play(); //SFX
     }
   }
 });
