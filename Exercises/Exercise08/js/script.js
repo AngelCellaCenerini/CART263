caac529c81faf14bf73ef786c3ee4a79a4c9fa44
Angel CC <angelcenerini@gmail.com>
2021-03-24 21:29:56 -0400

    E08: Started Exercise 08
    Included Phaser 3 script tag

---
+++ Exercises/Exercise08/js/script.js
@@ -0,0 +1,9 @@
+/**
+Desperately Seeking Sadness ++
+Angel Cella Cenerini
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
