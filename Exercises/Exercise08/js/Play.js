8ed68b4fbe8eb3f725537e5ceefdd27f660a6aed
Angel CC <angelcenerini@gmail.com>
2021-03-24 22:07:06 -0400

    E08: Set Up Keyboard Input

--- Exercises/Exercise08/js/Play.js
+++ Exercises/Exercise08/js/Play.js
@@ -8,9 +8,35 @@ class Play extends Phaser.Scene {
   create(){
     // Create avatar
     this.avatar = this.physics.add.image(400, 300, `avatar`);
+    // COnstrain Avatar to Canvas
+    this.avatar.setCollideWorldBounds(true);
+
+    // Access User's Keys
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  upload(){
+  update(){
+
+    // Move Avatar via Keys Input
+    // Left/Right Keys
+    if (this.cursors.left.isDown){
+      this.avatar.setAngularVelocity(-150);
+      console.log(`called`);
+    }
+    else if ( this.cursors.right.isDown ){
+      this.avatar.setAngularVelocity(150);
+    }
+    else {
+        this.avatar.setAngularVelocity(0);
+    }
+
+    // Up Key
+    if (this.cursors.up.isDown){
+      this.physics.velocityFromRotation(this.avatar.rotation, 200, this.avatar.body.acceleration);
+    }
+    else {
+      this.avatar.setAcceleration(0);
+    }
 
   }
 }
