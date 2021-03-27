358a6cb6da3b00c393c7d58b3f7927d2a9f0afa2
Angel CC <angelcenerini@gmail.com>
2021-03-26 21:10:42 -0400

    P02D: Started working on Prototype/Project02 Draft
    - basic Set Up

---
+++ Projects/Project02Draft/Project02_p5_Draft/js/FirstAvatar.js
@@ -0,0 +1,22 @@
+class FirstAvatar {
+  constructor(x, y, image){
+    this.x = x;
+    this.y = y;
+    this.image = image;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 4;
+  }
+
+  update(){
+    
+  }
+
+  move(){
+
+  }
+
+  display(){
+
+  }
+}
---
+++ Projects/Project02Draft/Project02_p5_Draft/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+Project02 Draft
+Angel Cella Cenerini
+
+Project02 Draft
+*/
+
+// User Initial Avatar
+let crosshairCursorImage = undefined;
+
+
+/**
+Description of preload
+*/
+function preload() {
+  crosshairCursorImage = loadImage(`assets/images/crosshair-cursor.png`);
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+createCanvas(750, 600);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+  // Color Background
+  background(20);
+}
