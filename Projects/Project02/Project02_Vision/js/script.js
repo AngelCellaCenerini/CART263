846eddc17f4845070be3bc52f071a55e8e2ddc0a
Angel CC <angelcenerini@gmail.com>
2021-04-20 03:05:11 -0400

    P02V: Experimenting with jQuery draggable event

--- Projects/Project02/Project02_Vision/js/script.js
+++ Projects/Project02/Project02_Vision/js/script.js
@@ -9,31 +9,33 @@ author, and this description to match your project!
 "use strict";
 
 // Discover Eyelid
-$(`#eyelid`).on(`mouseenter`, function(event) {
-  // Display Eyelid Borders via Mouse Input
-  $(this).addClass(`found`, 2000);
-  // Make Eyelid draggable
-  // $(this).draggable({
-  //   revert: "invalid", // when not dropped, the item will revert back to its initial position
-  //   containment: "document",
-
-    // Clone dragged letters
-    // helper: `clone`
-  });
+// $(`#eyelid`).on(`mouseenter`, function(event) {
+//   // Display Eyelid Borders via Mouse Input
+//   $(this).addClass(`found`, 2000);
+//   // Make Eyelid draggable
+//   // $(this).draggable({
+//   //   revert: "invalid", // when not dropped, the item will revert back to its initial position
+//   //   containment: "document",
+//
+//     // Clone dragged letters
+//     // helper: `clone`
+//   });
 
 
 $(`#eyelid`).draggable({
   axis: `y`,
   revert: true,
+  containment: `#eye-constrainer`,
+  scroll: false
   // revertDuration: 200
 });
 // $( "#eyelid" ).draggable( "revert", "revertDuration", 200 );
 
 // Hide Eyelid
-$(`#eyelid`).on(`mouseleave`, function(event) {
-  // Display Eyelid Borders via Mouse Input
-  $(this).removeClass(`found`, 2000);
-});
+// $(`#eyelid`).on(`mouseleave`, function(event) {
+//   // Display Eyelid Borders via Mouse Input
+//   $(this).removeClass(`found`, 2000);
+// });
 
 // Set Up Droppable Element
 // $( "#eyelid" ).draggable({ revert: "invalid" });
