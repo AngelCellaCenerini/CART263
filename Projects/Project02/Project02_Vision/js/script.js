e180295eb501420aa0570418809c5579b1575c08
Angel CC <angelcenerini@gmail.com>
2021-04-21 00:40:24 -0400

    P02V: Adjusted "Eyelid" mouse interactions
    - draggable (revert);
    - bottom border appearing/disappearing

--- Projects/Project02/Project02_Vision/js/script.js
+++ Projects/Project02/Project02_Vision/js/script.js
@@ -13,12 +13,10 @@ $(`#eyelid`).on(`mouseenter`, function(event) {
   // Display Eyelid Borders via Mouse Input
   $(this).addClass(`found`, 2000);
   // Make Eyelid draggable
-  // $(this).draggable({
-  //   revert: "invalid", // when not dropped, the item will revert back to its initial position
-  //   containment: "document",
-
-    // Clone dragged letters
-    // helper: `clone`
+  $(`#eyelid`).on(`mouseleave`, function(event) {
+    // Display Eyelid Borders via Mouse Input
+    $(this).removeClass(`found`, 200);
+  });
   });
 
 
@@ -27,31 +25,4 @@ $(`#eyelid`).draggable({
   revert: true,
   containment: `#eye-constrainer`,
   scroll: false
-  // revertDuration: 200
 });
-// $( "#eyelid" ).draggable( "revert", "revertDuration", 200 );
-
-// Hide Eyelid
-$(`#eyelid`).on(`mouseleave`, function(event) {
-  // Display Eyelid Borders via Mouse Input
-  $(this).removeClass(`found`, 2000);
-});
-
-// Set Up Droppable Element
-// $( "#eyelid" ).draggable({ revert: "invalid" });
-// $(`#eyelid` ).draggable({
-   // cancel: "a.ui-icon", // clicking an icon won't initiate dragging
-   // revert: "invalid", // when not dropped, the item will revert back to its initial position
-   // containment: "document",
-   // helper: "clone",
-   // cursor: "move"
- // });
-// $(`#eyelid`).droppable({
-//   drop: function(event, ui){
-//     // PLace letter in Draggable Element
-//     let upperEyelid = ui.draggable.text();
-//     $(this).append(upperEyelid);
-//     ui.draggable.draggable(`disable`);
-//     ui.draggable.removeClass(`found`);
-//   }
-// });
