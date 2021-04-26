d85c8083fe678114269e6ec8b607309bc4234435
Angel CC <angelcenerini@gmail.com>
2021-04-26 04:15:30 -0400

    P02M: Set Up Main mechanism
    User discovers (randomized)strings of text via mouse to then rewrite them in the input bar; if correctly rewritten, new strings will appear. Yet, the old ones won't deactivate, therefore the User must remember which are the new one.

--- Projects/Project02/Project02_Memory/js/memory-script.js
+++ Projects/Project02/Project02_Memory/js/memory-script.js
@@ -8,8 +8,31 @@ author, and this description to match your project!
 
 "use strict";
 
+// Store all Solutions
+let solutions = [
+ `vlck8jGzA8`,
+ `jrY6BLaYTe`,
+ `oPaRy93LVt`,
+ `buplCqPhs6`,
+ `dARkXta8Zm`
+]
+
+// Store all Solutions
+let strings = [
+ `#first-string`,
+ `#second-string`,
+ `#third-string`,
+ `#fourth-string`,
+ `#fifth-string`
+]
+
+let index = 0;
+let indexStrings = 0;
+
 // Discover Letters
-$(`#first-string`).on(`mouseover`, function(event) {
+let currentString = strings[indexStrings];
+
+$( `.all-strings`).on(`mouseover`, function(event) {
   // Display Text
   setTimeout( ()=>{
     $(this).animate({
@@ -28,3 +51,40 @@ $(`#first-string`).on(`mouseover`, function(event) {
   }, 1500);
 
 });
+
+// Guess Strings
+$(`#submit-guess`).on(`click`, rememberString);
+
+
+function rememberString(){
+  // Check Text Input
+  let input = $(`#text-input`).val();
+  // Check Current Solution/Correct Answer
+  let currentSolution = solutions[index];
+  // Check if User has guessed correctly
+  if (input === currentSolution){
+
+    console.log(`yes`);
+
+    // PLay SFX
+    // audio.play();
+    // Proceed to next Guess/Answer
+    index++;
+    // Apply Progress to String
+    indexStrings++;
+    updateString();
+    // $(currentString).remove();
+  }
+  else {
+console.log(`no`);
+  }
+}
+
+// }
+
+function updateString(){
+  // Apply Disappearing Effect on Prp=progressbar
+  let currentString = strings[indexStrings];
+  $(currentString).css(`visibility`, `visible`);
+
+}
