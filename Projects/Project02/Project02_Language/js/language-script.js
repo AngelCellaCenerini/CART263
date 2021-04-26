3ff2e8971afe6de29070049b7aac6d67f25622a5
Angel CC <angelcenerini@gmail.com>
2021-04-26 01:16:12 -0400

    P02L: Set Up Guessing mechanisms
    User tries to guess Icon's meaning via text/button input; a correct guess triggers reactions (obv)

--- Projects/Project02/Project02_Language/js/language-script.js
+++ Projects/Project02/Project02_Language/js/language-script.js
@@ -1,15 +1,135 @@
 /**
-Title of Project
-Author Name
+Project02 - Language Webpage
+Angel Cella Cenerini
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+User must guess what Icons means. It must understand and learn the language...
+of capitalism >:)
 */
 
 "use strict";
 
-// Code goes here
+// Store all Images Id
+let handSymbols = [
+`#okay`,
+`#okay2`,
+`#okay3`,
+`#okay4`,
+`#okay5`,
+`#okay6`,
+`#okay7`
+];
 
-setInterval( ()=>{
-  $(`#blinking-dot`).toggleClass(`invisible`);
-}, 700);
+// Store all Solutions
+let solutions = [
+ `okay`,
+ `okay2`,
+ `okay3`,
+ `okay4`,
+ `okay5`,
+ `okay6`,
+ `okay7`
+]
+
+// Solutions Array Index
+let index = 0;
+// Hand Symbols Array Index
+let iconIndex = 0;
+
+// Progressbar Values
+// Default Value
+let currentProgressbarValue = 0;
+// Added Value
+let addedProgressbarValue = 10;
+
+// Create SFX
+const audio = new Audio("assets/sounds/bark.wav");
+
+// Display Progressbar
+displayProgressbar();
+
+// Randomized Blinking Effect
+visualEffects();
+
+// Guess Hand Symbol
+// Insert Guess
+$(`#sumbit-guess`).on(`click`, guessHandMeaning);
+
+
+
+
+// Functions
+function displayProgressbar(){
+  // Display Progressbar
+  $( function() {
+    $( "#progressbar" ).progressbar({
+      value: currentProgressbarValue
+    });
+  } );
+}
+
+function visualEffects(){
+  // Visual Effects
+  setInterval( ()=>{
+    $(`#blinking-dot`).hide();
+  }, 700);
+
+  setInterval( ()=>{
+    $(`.blinking-effect-1`).hide();
+  }, 600);
+
+  setInterval( ()=>{
+    $(`.blinking-effect-2`).hide();
+  }, 900);
+
+  setInterval( ()=>{
+    $(`#first-varied-data`).hide();
+  }, 3000);
+
+  setInterval( ()=>{
+    $(`#second-varied-data`).show();
+  }, 3000);
+
+  setInterval( ()=>{
+    $(`#first-numeric-data`).hide();
+  }, 2000);
+
+  setInterval( ()=>{
+    $(`#second-numeric-data`).show();
+  }, 2000);
+}
+
+function guessHandMeaning(){
+// Check Text Input
+let input = $(`#text-input`).val();
+// Check Current Solution/Correct Answer
+let currentSolution = solutions[index];
+// Check if User has guessed correctly
+if (input === currentSolution){
+
+  // PLay SFX
+  audio.play();
+  // Proceed to next Guess/Answer
+  index++;
+  // Apply Progress to Progressbar
+  updateProgressBar();
+  // Apply Progress to Icon
+  updateHandIcon();
+}
+else {
+  console.log(`nope`);
+}
+}
+
+function updateProgressBar(){
+  // Update Progressbar after User guesses correctly
+  currentProgressbarValue += addedProgressbarValue;
+    $("#progressbar").progressbar(`value`, currentProgressbarValue);
+}
+
+function updateHandIcon(){
+  // Apply Disappearing Effect on Prp=progressbar
+  let currentIcon = handSymbols[iconIndex];
+    $(currentIcon).effect( "blind", "slow" );
+      iconIndex++;
+}
+//
