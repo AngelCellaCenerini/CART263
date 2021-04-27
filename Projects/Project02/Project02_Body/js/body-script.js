c113fb5c4327b55bab93c2e21da52a9d11dd7e27
Angel CC <angelcenerini@gmail.com>
2021-04-27 01:58:35 -0400

    P02B: Set up draggable/droppable mechanisms
    User must drop "statue part" on statue silhouette to complete it and make button appear.

--- Projects/Project02/Project02_Body/js/body-script.js
+++ Projects/Project02/Project02_Body/js/body-script.js
@@ -8,4 +8,31 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+// Keep Track of Statue Progress
+let collectedParts = 0;
+
+
+// Drag Statue Pieces
+$(`#statue-section`).on(`mouseover`, function(event) {
+  $(this).draggable({
+    // Constrain Movement
+    containment: `#maze`,
+    // Revert Movement if part is not dropped upon statue
+    revert: "invalid"
+  });
+});
+
+// Set Up Droppable Element
+$(`#statue-silhouette`).droppable({
+  // PLace Statue part upon statue
+  drop: function(event, ui){
+    // Hide part once dropped
+    $(`#statue-section`).hide();
+    // Eventually complete Statue
+    if ( collectedParts >= 0){
+      $(`#statue-bust`).css( `visibility`, `visible`);
+    }
+
+    collectedParts++;
+    }
+});
