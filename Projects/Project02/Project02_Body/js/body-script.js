cebe0bd9cb0bca5d58837089972d6786e0c2d903
Angel CC <angelcenerini@gmail.com>
2021-04-27 03:24:48 -0400

    P02B: Program now keeps track of User's Progress

--- Projects/Project02/Project02_Body/js/body-script.js
+++ Projects/Project02/Project02_Body/js/body-script.js
@@ -10,46 +10,95 @@ author, and this description to match your project!
 
 // Keep Track of Statue Progress
 let collectedParts = 0;
+let index = 0;
+let parts = [
+  `#bust-section`,
+  `#left-leg-section`,
+  `#head-section`,
+  `#right-leg-section`
+];
 
-// Loop function
-slideRightHorizontalWalls();
-slideLeftHorizontalWalls();
- slideVerticalWalls();
+let currentPart = parts[index];
 
-// Drag Statue Pieces
-$(`#statue-section`).on(`mouseover`, function(event) {
-  $(this).draggable({
-    // Constrain Movement
-    containment: `#maze`,
-    // Revert Movement if part is not dropped upon statue
-    revert: "invalid"
+
+
+// Sliding Animations
+slidingWalls();
+
+dragPiece();
+
+dropPiece();
+
+function dragPiece(){
+  // Drag Statue Pieces
+  $(`.statue-section`).on(`mouseover`, function(event) {
+    $(this).draggable({
+
+      // Constrain Movement
+      containment: `#maze`,
+      // Revert Movement if part is not dropped upon statue
+      revert: "invalid"
+    });
   });
-});
+}
 
-// Set Up Droppable Element
-$(`#statue-silhouette`).droppable({
-  // PLace Statue part upon statue
-  drop: function(event, ui){
-    // Hide part once dropped
-    $(`#statue-section`).hide();
-    // Eventually complete Statue
-    if ( collectedParts >= 0){
-      $(`#statue-bust`).css( `visibility`, `visible`);
-    }
+function dropPiece(){
+  // Set Up Droppable Element
+  $(`#statue-silhouette`).droppable({
+    // PLace Statue part upon statue
+    drop: function(event, ui){
 
-    collectedParts++;
-    }
-});
+      // Hide part once dropped
+      $(currentPart).hide();
 
+      // Proceed to Next Statue Section
+      index++;
+      currentPart = parts[index];
+
+      // Display Next Statue Section
+      $(currentPart).css( `visibility`, `visible`);
+
+      // Eventually complete Statue
+      if ( collectedParts >= 0){
+        // Display Bust
+        $(`#statue-bust`).css( `visibility`, `visible`);
+        if (collectedParts = 1){
+          // Display Left Leg
+          $(`#statue-left-leg`).css( `visibility`, `visible`);
+        }
+        if (collectedParts = 2){
+          // Display Head
+          $(`#statue-head`).css( `visibility`, `visible`);
+        }
+        if (collectedParts = 3){
+          // Display Right Leg
+          $(`#statue-right-leg`).css( `visibility`, `visible`);
+        }
+      }
+      collectedParts++;
+      }
+  });
+}
+
+
+
+
+function slidingWalls(){
+  // Sliding Animations
+  // Looped animations
+  slideRightHorizontalWalls();
+  slideLeftHorizontalWalls();
+  slideVerticalWalls();
+}
 function slideRightHorizontalWalls(){
   $(`#left-side`).animate({
     "left": `75%`
   }, {
-    duration: 3000,
+    duration: 2000,
     complete: function(){
       $(`#left-side`).animate({
         "left": `0%`
-    }, 3000);
+    }, 2000);
 
     // Loop function
     slideRightHorizontalWalls();
@@ -61,11 +110,11 @@ function slideLeftHorizontalWalls(){
   $(`#right-side`).animate({
     "left": `0%`
   }, {
-    duration: 3500,
+    duration: 1700,
     complete: function(){
       $(`#right-side`).animate({
         "left": `75%`
-    }, 3500);
+    }, 1700);
 
     // Loop function
     slideLeftHorizontalWalls();
@@ -77,11 +126,11 @@ function slideVerticalWalls(){
   $(`#vertical-walls`).animate({
     "top": `50%`
   }, {
-    duration: 3500,
+    duration: 1500,
     complete: function(){
       $(`#vertical-walls`).animate({
         "top": `0%`
-    }, 3500);
+    }, 1500);
 
     // Loop function
     slideVerticalWalls();
