fe1e449d8cdeee227e90e6c35da650dc2c82c030
Angel CC <angelcenerini@gmail.com>
2021-04-27 02:19:39 -0400

    P02B: Set Up Sliding animation
    ""Maze"" 's walls slide across the screen to physically block User from collecting statue parts.

--- Projects/Project02/Project02_Body/js/body-script.js
+++ Projects/Project02/Project02_Body/js/body-script.js
@@ -11,6 +11,10 @@ author, and this description to match your project!
 // Keep Track of Statue Progress
 let collectedParts = 0;
 
+// Loop function
+slideRightHorizontalWalls();
+slideLeftHorizontalWalls();
+ slideVerticalWalls();
 
 // Drag Statue Pieces
 $(`#statue-section`).on(`mouseover`, function(event) {
@@ -36,3 +40,51 @@ $(`#statue-silhouette`).droppable({
     collectedParts++;
     }
 });
+
+function slideRightHorizontalWalls(){
+  $(`#left-side`).animate({
+    "left": `75%`
+  }, {
+    duration: 3000,
+    complete: function(){
+      $(`#left-side`).animate({
+        "left": `0%`
+    }, 3000);
+
+    // Loop function
+    slideRightHorizontalWalls();
+  }
+  });
+}
+
+function slideLeftHorizontalWalls(){
+  $(`#right-side`).animate({
+    "left": `0%`
+  }, {
+    duration: 3500,
+    complete: function(){
+      $(`#right-side`).animate({
+        "left": `75%`
+    }, 3500);
+
+    // Loop function
+    slideLeftHorizontalWalls();
+  }
+  });
+}
+
+function slideVerticalWalls(){
+  $(`#vertical-walls`).animate({
+    "top": `50%`
+  }, {
+    duration: 3500,
+    complete: function(){
+      $(`#vertical-walls`).animate({
+        "top": `0%`
+    }, 3500);
+
+    // Loop function
+    slideVerticalWalls();
+  }
+  });
+}
