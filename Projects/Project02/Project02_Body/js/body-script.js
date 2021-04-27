8f6723e6931d7bd9b8dcf31fb3c01226a22d712b
Angel CC <angelcenerini@gmail.com>
2021-04-26 22:49:04 -0400

    P02B: Started Project02 Body Webpage
    Not the most fascinating title, sorry :/

---
+++ Projects/Project02/Project02_Body/js/body-script.js
@@ -0,0 +1,11 @@
+/**
+Project02 Body Webpage
+Angel Cella Cenerini
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+// Code goes here
